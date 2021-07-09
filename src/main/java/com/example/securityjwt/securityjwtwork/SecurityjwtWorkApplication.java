a54e3cf6f1d514ed1e3fc5e1690e9b57f77c9bc5
devsunset <devsunset@gmail.com>
2021-07-09 14:44:43 +0900

    init

---
+++ src/main/java/com/example/securityjwt/securityjwtwork/SecurityjwtWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwt.securityjwtwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityjwtWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityjwtWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityjwt/securityjwtwork/SecurityjwtWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwt.securityjwtwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityjwtWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
